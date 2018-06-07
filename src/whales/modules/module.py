a6122889ece089166f480d7648b9f2f5f7b5d294
Sergio Liberman B <sliberman@alges.cl>
2018-06-07 19:54:02 -0400

    Added parameters and logger to module class

--- src/whales/modules/module.py
+++ src/whales/modules/module.py
@@ -1,3 +1,18 @@
-class Module():
+import logging
+
+
+class Module:
     """Generic module with inheritable methods"""
-    pass
+    def __init__(self, logger=logging.getLogger(__name__)):
+        self.logger = logger
+
+        self._parameters = {}
+
+    @property
+    def parameters(self):
+        return self._parameters
+
+    @parameters.setter
+    def parameters(self, parameters: dict):
+        for p, v in parameters.items():
+            self._parameters[p] = v
