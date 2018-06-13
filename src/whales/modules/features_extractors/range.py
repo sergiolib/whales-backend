2958b404a34cc2caa612b90955726a93cf309a0d
Sergio Liberman B <sliberman@alges.cl>
2018-06-13 18:01:31 -0400

    Added range feature and added more constrains in tests

---
+++ src/whales/modules/features_extractors/range.py
@@ -0,0 +1,21 @@
+import numpy as np
+from whales.modules.features_extractors.feature_extraction import FeatureExtraction
+
+
+class Range(FeatureExtraction):
+    def __init__(self, logger=None):
+        super(Range, self).__init__(logger)
+        self.needs_fitting = False
+        self.description = "Range"
+        self.parameters = {
+
+        }
+
+    def method_transform(self, data):
+        min_value = np.min(data, axis=1)
+        max_value = np.max(data, axis=1)
+        range_value = max_value - min_value
+        return range_value.reshape(-1, 1)
+
+
+PipelineMethod = Range
--- test/test_features.py
+++ test/test_features.py
@@ -4,6 +4,7 @@ from whales.modules.features_extractors.mfcc import PipelineMethod as MFCC
 from whales.modules.features_extractors.identity import PipelineMethod as Identity
 from whales.modules.features_extractors.zero_crossing_rate import PipelineMethod as ZeroCrossingRate
 from whales.modules.features_extractors.min import PipelineMethod as Min
+from whales.modules.features_extractors.range import PipelineMethod as Range
 
 
 def generate_data(n, d):
@@ -32,6 +33,8 @@ def test_zero_crossing_rate():
     data = generate_data(10000, 2500) - 0.5
     f = ZeroCrossingRate()
     t = f.transform(data=data)
+    assert t.shape[0] == data.shape[0]
+    assert t.shape[1] == 1
     assert f.description != ""
     assert t.ndim == 2
 
@@ -40,5 +43,17 @@ def test_min():
     data = generate_data(10000, 2500) - 0.5
     f = Min()
     t = f.transform(data=data)
+    assert t.shape[0] == data.shape[0]
+    assert t.shape[1] == 1
+    assert f.description != ""
+    assert t.ndim == 2
+
+
+def test_range():
+    data = generate_data(10000, 2500) - 0.5
+    f = Range()
+    t = f.transform(data=data)
+    assert t.shape[0] == data.shape[0]
+    assert t.shape[1] == 1
     assert f.description != ""
     assert t.ndim == 2
