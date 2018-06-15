17bbfe0f8944eb540d4c557bbbca56b050a85213
Sergio Liberman B <sliberman@alges.cl>
2018-06-14 22:29:16 -0400

    Pending changes in clustering directory. Nothing really changes.

--- src/whales/modules/clustering/clustering.py
+++ src/whales/modules/clustering/clustering.py
@@ -2,12 +2,12 @@ from whales.modules.module import Module
 
 
 class Clustering(Module):
+    def fit(self, data):
+        # Do not re implement this method, instead implement method_
+        self.method_fit(data)
+
     def method_fit(self, data):
         raise NotImplementedError
-
-    def fit(self, data):
-        # Do not re implement this method, instead implement method_fit
-        return self.method_fit(data)
 
     def method_predict(self, data):
         raise NotImplementedError
