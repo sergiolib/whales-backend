500f350fed5b0d914cff57fbf7261741445b4124
Sergio Liberman Bronfman <sliberman@alges.cl>
2018-06-18 13:01:49 -0400

    Fixed metadata file type discrepancy. Now it is always a dict

--- src/whales/modules/datafiles/datafiles.py
+++ src/whales/modules/datafiles/datafiles.py
@@ -22,7 +22,7 @@ class Datafile(Module):
         """Do not actually load the data. Instead, save the access information."""
         self.file_name = file_name
         self.formatter = formatter
-        self.metadata = formatter.read_metadata(file_name)
+        self.metadata = {self.data.name: formatter.read_metadata(file_name)}
         return self
 
     def save_data(self, file_name: str, formatter):
