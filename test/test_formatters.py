cfd0890aa6ef6c747b9891a35f60751c51ef490e
Sergio Liberman Bronfman <sliberman@alges.cl>
2018-06-18 18:08:38 -0400

    Fixed test that wasn't passing because of changing Series and DataFrame

--- test/test_formatters.py
+++ test/test_formatters.py
@@ -15,17 +15,17 @@ class TestAIFFormatter:
     def test_read(self):
         filename = get_filename()
         formatter = AIFFormatter()
-        series = formatter.read(filename)
-        assert type(series) is pd.Series
-        assert len(series) > 0
-        assert type(series.index) is pd.DatetimeIndex
+        df = formatter.read(filename)
+        assert type(df) is pd.DataFrame
+        assert len(df) > 0
+        assert type(df.index) is pd.DatetimeIndex
 
         new_filename = "same_file_new_name.aif"
         rename(filename, "same_file_new_name.aif")
-        series = formatter.read(new_filename)
-        assert type(series) is pd.Series
-        assert len(series) > 0
-        assert type(series.index) is pd.RangeIndex
+        df = formatter.read(new_filename)
+        assert type(df) is pd.DataFrame
+        assert len(df) > 0
+        assert type(df.index) is pd.RangeIndex
 
     def test_read_metadata(self):
         filename = get_filename()
