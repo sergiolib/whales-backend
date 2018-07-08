35c72d674401ff67da57c06f039ebb1668ea6068
Sergio Liberman B <sliberman@alges.cl>
2018-07-08 17:26:24 -0400

    Fixed serialization of timestamps

--- src/whales/modules/formatters/json.py
+++ src/whales/modules/formatters/json.py
@@ -1,16 +1,18 @@
 import json
 from os.path import splitext
 
+from whales.utilities.json import WhalesEncoder, WhalesDecoder
+
 
 class JSONMetadataMixin:
     """Mixin for using where format doesn't have built in metadata storage"""
     def read_metadata(self, metadata_filename):
         metadata_filename = correct_metadata_filename(metadata_filename)
-        return json.load(open(metadata_filename, mode="r"))
+        return json.load(open(metadata_filename, mode="r"), cls=WhalesDecoder)
 
     def write_metadata(self, metadata_filename, metadata):
         metadata_filename = correct_metadata_filename(metadata_filename)
-        json.dump(metadata, open(metadata_filename, mode="w"))
+        json.dump(metadata, open(metadata_filename, mode="w"), cls=WhalesEncoder)
 
 
 def correct_metadata_filename(metadata_filename):
--- src/whales/utilities/json.py
+++ src/whales/utilities/json.py
@@ -1,6 +1,6 @@
 import json
 from numpy import ndarray, array
-from pandas import Series
+from pandas import Series, Timestamp
 
 
 class WhalesEncoder(json.JSONEncoder):
@@ -10,6 +10,9 @@ class WhalesEncoder(json.JSONEncoder):
 
         if isinstance(obj, Series):
             return {"type": "series", "data": obj.tolist(), "index": obj.index.tolist()}
+
+        if isinstance(obj, Timestamp):
+            return {"type": "timestamp", "data": obj.timestamp()}
 
         # Let the base class default method raise the TypeError
         return json.JSONEncoder.default(self, obj)
@@ -27,5 +30,8 @@ class WhalesDecoder(json.JSONDecoder):
 
                 if obj["type"] == "series":
                     return Series(obj["data"], index=obj["index"])
+
+                if obj["type"] == "timestamp":
+                    return Timestamp.fromtimestamp(obj["data"])
 
         return obj
