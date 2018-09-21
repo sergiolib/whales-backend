1825946b3abe0c19688e09fced6e874fce8a5507
Sergio Liberman Bronfman <sliberman@alges.cl>
2018-09-21 20:25:21 +0200

    Range working

--- src/whales/modules/features_extractors/range.py
+++ src/whales/modules/features_extractors/range.py
@@ -14,14 +14,36 @@ class Range(FeatureExtraction):
 
     def method_transform(self):
         data_file = self.all_parameters["data_file"]
-        data = data_file.data.values
-        min_value = np.nanmin(data, axis=1)
-        max_value = np.nanmax(data, axis=1)
-        res = max_value - min_value
-        data = pd.DataFrame(res, index=data_file.data.index, columns=["range"])
-        fdf = FeatureDataFile()
-        fdf._data = data
-        return fdf
+        # Disable using this in settings without sliding windows for now
+        if "window_width" not in data_file.metadata:
+            self.logger.error("Window width was not specified")
+            raise AttributeError
+        if "overlap" not in data_file.metadata:
+            self.logger.error("Overlap was not specified")
+            raise AttributeError
+
+        fs = data_file.sampling_rate
+        win = int(data_file.metadata.get("window_width", data_file.duration.seconds) * fs)
+        step = int(win * (1.0 - data_file.metadata.get("overlap", 0.0)))
+        data = data_file.data.astype(float)
+        st = 0
+        en = st + step
+        res = []
+        while True:
+            if en > len(data):
+                en = len(data)
+            row = data.iloc[st:en]
+            res.append(row.max() - row.min())
+            st = en
+            if en == len(data):
+                break
+            en = en + step
+        f = FeatureDataFile("min")
+        f.data = np.array(res).reshape(-1, 1)
+        inds = pd.date_range(data.index[0], data.index[-1], len(res))
+        f.data.index = inds
+        f.data = f.data.dropna()
+        return f
 
 
 PipelineMethod = Range
