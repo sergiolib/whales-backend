4489c3161efa55167cdf330f2f16a73fe14e13e2
Sergio Liberman Bronfman <sliberman@alges.cl>
2018-09-21 20:24:05 +0200

    Min working

--- src/whales/modules/features_extractors/min.py
+++ src/whales/modules/features_extractors/min.py
@@ -14,12 +14,35 @@ class Min(FeatureExtraction):
 
     def method_transform(self):
         data_file = self.all_parameters["data_file"]
-        data = data_file.data.values
-        res = np.nanmin(data, axis=1).reshape(-1, 1)
-        data = pd.DataFrame(res, index=data_file.data.index, columns=["min"])
-        fdf = FeatureDataFile()
-        fdf._data = data
-        return fdf
+        # Disable using this in settings without sliding windows for now
+        if "window_width" not in data_file.metadata:
+            self.logger.error("Window width was not specified")
+            raise AttributeError
+        if "overlap" not in data_file.metadata:
+            self.logger.error("Overlap was not specified")
+            raise AttributeError
+
+        fs = data_file.sampling_rate
+        win = int(data_file.metadata.get("window_width", data_file.duration.seconds) * fs)
+        step = int(win * (1.0 - data_file.metadata.get("overlap", 0.0)))
+        data = data_file.data.astype(float)
+        st = 0
+        en = st + step
+        res = []
+        while True:
+            if en > len(data):
+                en = len(data)
+            res.append(data.iloc[st:en].min())
+            st = en
+            if en == len(data):
+                break
+            en = en + step
+        f = FeatureDataFile("min")
+        f.data = np.array(res).reshape(-1, 1)
+        inds = pd.date_range(data.index[0], data.index[-1], len(res))
+        f.data.index = inds
+        f.data = f.data.dropna()
+        return f
 
 
 PipelineMethod = Min
