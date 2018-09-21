da3e10b2fd848a0c363b65339b8235ccab456b2c
Sergio Liberman Bronfman <sliberman@alges.cl>
2018-09-21 20:14:52 +0200

    Identity ready

--- src/whales/modules/features_extractors/identity.py
+++ src/whales/modules/features_extractors/identity.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from whales.modules.data_files.feature import FeatureDataFile
 from whales.modules.features_extractors.feature_extraction import FeatureExtraction
@@ -11,15 +12,31 @@ class Identity(FeatureExtraction):
         self.parameters = {}
 
     def method_transform(self):
-        new_data_file = FeatureDataFile()
         data_file = self.all_parameters["data_file"]
-        # Caution with nan values as they cannot go into the classifiers
-        # Removing it in the mean time...
-
-        data = data_file.data.dropna()
-        data.columns = [f"identity_{i}" for i, _ in enumerate(data.columns)]
-        new_data_file._data = data
-        return new_data_file
+        if "window_width" not in data_file.metadata:
+            self.logger.error("Window width was not specified")
+            raise AttributeError
+        if "overlap" not in data_file.metadata:
+            self.logger.error("Overlap was not specified")
+            raise AttributeError
+        fs = data_file.sampling_rate
+        win = int(data_file.metadata["window_width"] * fs)
+        step = int(win * (1.0 - data_file.metadata["overlap"]))
+        data = data_file.data.astype(float)
+        st = 0
+        en = st + step
+        res = []
+        while True:
+            if en > len(data):
+                break
+            res.append(data.iloc[st:en].values)
+            st = en
+            en = en + step
+        fdf = FeatureDataFile("mfcc")
+        fdf.data = np.vstack(res)
+        indexes = pd.date_range(data_file.data.index[0], data_file.data.index[-1], periods=len(data_file.data)//step)
+        fdf._data.index = indexes
+        return fdf
 
 
 PipelineMethod = Identity
