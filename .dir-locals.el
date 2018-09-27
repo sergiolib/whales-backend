c62091609e95bc30db342f963d5ca2c9a5149e6f
Sergio Liberman Bronfman <sliberman@alges.cl>
2018-09-27 17:11:42 +0200

    Removed emacs file

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((python-mode
-  (elpy-project-root . "/home/sliberman/Documents/src/whales/backend/src")
-  (pyvenv-activate . "/home/sliberman/Documents/src/whales/venv")))
-
-
