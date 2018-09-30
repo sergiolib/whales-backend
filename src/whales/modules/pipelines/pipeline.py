1dd80ebb88aff76a56b599f511d08b1989981441
Sergio Liberman Bronfman <sliberman@alges.cl>
2018-10-01 01:39:46 +0200

    Further fix to logging issues

--- src/whales/modules/pipelines/pipeline.py
+++ src/whales/modules/pipelines/pipeline.py
@@ -61,6 +61,7 @@ class Pipeline(Module):
                 self.logger.setLevel(logging.INFO)
 
             self.loaders.logger = self.logger
+            self.loaders.instructions_set.logger = self.logger
 
         self.logger.info("Parameters loaded correctly")
 
